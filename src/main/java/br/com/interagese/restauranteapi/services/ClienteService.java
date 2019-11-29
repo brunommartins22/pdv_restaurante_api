c1e84a1e32ca5589fbc6e48b94ad6ccc272c7e48
dinael <tadsdinael@gmail.com>
2019-11-29 14:24:17 -0200

    padrao

--- src/main/java/br/com/interagese/restauranteapi/services/ClienteService.java
+++ src/main/java/br/com/interagese/restauranteapi/services/ClienteService.java
@@ -7,6 +7,10 @@ package br.com.interagese.restauranteapi.services;
 
 import br.com.interagese.padrao.rest.util.PadraoService;
 import br.com.interagese.restaurantemodel.models.Cliente;
+import br.com.interagese.restaurantemodel.models.Endereco;
+import br.com.interagese.restaurantemodel.models.Fornecedor;
+import br.com.interagese.restaurantemodel.models.Telefone;
+import java.util.Date;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +20,31 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService extends PadraoService <Cliente>{
+    
+    @Override
+    public Cliente create(Cliente obj) throws Exception {
+        try {
+            setID(obj);
+
+            obj.getAtributoPadrao().setDataAlteracao(new Date());
+            obj.getPessoa().setAtributoPadrao(obj.getAtributoPadrao());
+            
+            for( Endereco end: obj.getPessoa().getListEndereco()) {
+                end.setAtributoPadrao(obj.getAtributoPadrao());
+            }
+            
+            for ( Telefone tel: obj.getPessoa().getListTelefone()){
+                 tel.setAtributoPadrao(obj.getAtributoPadrao());
+            }
+            em.persist(obj);
+
+            return obj;
+        } catch (IllegalArgumentException ex) {
+            return null;
+        }
+    }
+
+    
+    
     
 }
